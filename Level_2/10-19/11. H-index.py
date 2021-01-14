11befe4dc66fc4e100ab0b882d39b1eb75b52316
gujinzzang <jenny1338@naver.com>
2021-01-14 11:26:33 +0000

    sorted

---
+++ Level_2/10-19/11. H-index.py
@@ -0,0 +1,18 @@
+#2021/01/14
+
+def solution(citations):
+    if min(citations) >= len(citations): 
+      answer = len(citations)
+    else: answer = 0
+    
+    citations = sorted(citations)
+
+    for i in range(min(citations), max(citations) + 1):
+        left = 0
+        right = 0
+        for j in citations:
+            if j <= i: left += 1
+            if j >= i: right += 1
+        if (left <= i) & (right >= i):
+            answer = max(answer, i)
+    return answer
